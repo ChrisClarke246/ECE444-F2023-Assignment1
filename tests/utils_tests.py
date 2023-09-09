38643477c19c344d911ff6505b88941513637b32
CHRISTIAN_MCINTOSH_CLARKE <clarkechristian5@gmail.com>
2023-09-09 11:36:54 -0400

    test formatter

--- tests/utils_tests.py
+++ tests/utils_tests.py
@@ -13,3 +13,16 @@ def test_reversd_floats(float_num):
 def test_reversd_int(int_num):
     res = Utils.reversed(int_num)
     assert res == 321
+
+def test_formatter_string(str_num):
+    with pytest.raises(AssertionError):
+        Utils.formatter(str_num)
+
+def test_formatter_float(float_num):
+    with pytest.raises(AssertionError):
+        Utils.formatter(float_num)
+    
+def test_formatter_int(int_num):
+    res_binary, res_base_8 = Utils.formatter(int_num)
+    assert res_binary == 0b111011
+    assert res_base_8 == 0o173
